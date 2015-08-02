8978c0c4fb855281d1f838cbb554184926a62f75
Liem Ly Quan <liemlyquan@gmail.com>
2015-08-02 08:34:57 +0700

    Allows to retrieve user calories in the current week

--- hackademics/app/urls.py
+++ hackademics/app/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.categories, name='categories'),
-    url(r'^category=(?P<category_id>[0-9]+)/$', views.category, name='category'),
-    url(r'^workout=(?P<workout_id>[0-9]+)/$', views.workout, name='workout'),
-    url(r'^dump_data_path=(?P<user_id>[0-9]+)/$', views.dump_user_data, name='data_path'),
-    url(r'^user=(?P<user_id>[0-9]+)/$', views.user_info, name='user_info'),
+    url(r'^category=(?P<category_id>[0-9]+)/$', views.category),
+    url(r'^workout=(?P<workout_id>[0-9]+)/$', views.workout),
+    url(r'^dump_user_data=(?P<user_id>[0-9]+)/$', views.dump_user_data),
+    url(r'^user=(?P<user_id>[0-9]+)/$', views.user_info),
+    url(r'^total_calo_week=(?P<user_id>[0-9]+)/$', views.total_calo_week),
 ]
--- hackademics/app/views.py
+++ hackademics/app/views.py
@@ -1,9 +1,9 @@
 from random import randrange
+import datetime
 from django.http import HttpResponse
 from models import Category, Workout, User
 import json
-import calendar
-import collections
+from calendar import monthrange
 
 
 def categories(request):
@@ -29,13 +29,18 @@ def dump_user_data(request, user_id):
     dump_dict = {}
     for i in range(1000):
         month = randrange(1, 13)
-        cal = calendar.Calendar()
-        day = randrange(1,28)
+        day = monthrange(2015, month)
         hour = randrange(6, 18)
         minute = randrange(0, 59)
-        time = str(hour) + ":" + str(minute)
-        workout_id = randrange(1,13)
-        calories = randrange(100,500)
+        hour_string = str(hour)
+        minute_string = str(minute)
+        if hour < 10:
+            hour_string = "0" + hour_string
+        if minute < 10:
+            minute_string = "0" + minute_string
+        time = hour_string + ":" + minute_string
+        workout_id = randrange(1, 13)
+        calories = randrange(100, 500)
 
         if month not in dump_dict.keys():
             dump_dict[month] = {}
@@ -46,6 +51,7 @@ def dump_user_data(request, user_id):
 
         dump_dict[month][day][time]["workoutId"] = workout_id
         dump_dict[month][day][time]["calories"] = calories
+
     user.history = dump_dict
     user.save()
     return HttpResponse("Dump data successfully")
@@ -53,4 +59,40 @@ def dump_user_data(request, user_id):
 
 def user_info(request, user_id):
     user = User.objects.get(pk=user_id).as_json()
-    return HttpResponse(json.dumps(user), content_type="application/json")
+    return HttpResponse(json.dumps(user, sort_keys=True), content_type="application/json")
+
+
+def total_calo_week(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user_history = user.history
+
+    now = datetime.datetime.now()
+    now_month = now.month
+    now_day = now.day
+    now_day_of_week = now.weekday()
+    recent_list = {}
+    date_to_traceback = now_day_of_week - now_day
+
+    if now_day < date_to_traceback:
+        previous_month = now_month - 1
+        previous_days_to_calculate = monthrange(2015, previous_month)[1] - date_to_traceback
+        for i in range(previous_days_to_calculate, monthrange(2015, previous_month)[1]+1):
+            sum_of_day = 0
+            try:
+                for j in user_history[str(previous_month)][str(i)].keys():
+                    sum_of_day += user_history[str(previous_month)][str(i)][str(j)]['calories']
+                recent_list[str(previous_month) + "-" + str(i)] = sum_of_day
+            except KeyError:
+                recent_list[str(previous_month) + "-" + str(i)] = 0
+    for i in range(1, now_day+1):
+        sum_of_day = 0
+        try:
+            for j in user_history[str(now_month)][str(i)].keys():
+                sum_of_day += user_history[str(now_month)][str(i)][str(j)]['calories']
+            recent_list[str(now_month) + "-" + str(i)] = sum_of_day
+        except KeyError:
+            recent_list[str(now_month) + "-" + str(i)] = 0
+    return HttpResponse(json.dumps(recent_list, sort_keys=True), content_type="application/json")
+    # return HttpResponse(monthrange(2015,7)[1])
+# user_history["8"]["1"]["16:31"]["calories"]
+
